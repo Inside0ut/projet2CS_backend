e9073d7faafc3da8eaccd54bacc241d302f3fcf1
unknown <hs_boushaba@esi.dz>
2021-06-29 02:46:07 +0100

    fix:add chassis number in create

--- details_tech_bylocation/src/routes/index.ts
+++ details_tech_bylocation/src/routes/index.ts
@@ -8,7 +8,7 @@ router.get('/', get);
 router.post('/AddTechDetails', add_techDetails_ByLocation);
 router.get('/getVehicleInformations', getVehicleInfos);
 router.get('/getRentalInfo', getRentalInfo);
-router.post('/getRentalInfo', createVehicleState);
+router.post('/createVehiculeState', createVehicleState);
 
 
 export default router;
