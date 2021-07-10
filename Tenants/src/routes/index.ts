13cf32c00eb0eb7a00c61a0ea3bbae6b6c57c759
Inside0ut <ha_elfraihi@esi.dz>
2021-07-10 17:26:08 +0100

    minor addons

--- Tenants/src/routes/index.ts
+++ Tenants/src/routes/index.ts
@@ -3,6 +3,6 @@ import {getTenants,banTenantAccount} from '../controllers/Tenant';
 
 const router = Router();
 router.get('/tenant/all',getTenants) ;/**get all tenants */
-router.put('/banTenantAccount',banTenantAccount) ;/**get all tenants */
+router.put('/banTenantAccount',banTenantAccount) ;
 
 export default router;
